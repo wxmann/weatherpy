5c4374822075f28ee850bc7b860bc5e3b69b82fb
wxmann <wxfreakk91@gmail.com>
2017-01-31 02:02:23 -0800

    Plot satellite image "title".

--- examples/goessat_ex.py
+++ examples/goessat_ex.py
@@ -1,10 +1,10 @@
 from datetime import date, datetime
 
 import matplotlib.pyplot as plt
+from matplotlib import patheffects
 
 from weatherpy import colortables
 from weatherpy import goessat
-from weatherpy import mapproj
 from weatherpy import plotutils
 
 
@@ -13,13 +13,19 @@ def latest_east_conus_ir():
     req = goessat.DataRequest('WV', 'EAST-CONUS_4km', date(2017, 1, 20))
 
     with req[datetime(2017, 1, 20, 5, 45)] as plotter:
-        mapper = mapproj.EquidistantCylindricalMapper()
-        mapper.extent = (-110, -70, 20, 50)
+        mapper = plotter.mapper
+        mapper.extent = (-112.5, -70, 17.5, 52.5)
+        mapper.draw_coastlines()
+        mapper.draw_borders()
+        mapper.draw_states()
 
         plotter.make_plot(mapper=mapper, colortable=colortable)
-        title_text = 'GOES East {} Satellite {}'.format(plotter.sattype, plotter.timestamp.strftime('%m/%d/%Y %H:%MZ'))
-        plt.title(title_text)
-        plotutils.plot_legend(colortable)
+        title_text = 'GOES {}\n{}'.format(plotter.sattype, plotter.timestamp.strftime('%Y %b %d\n%H:%M UTC'))
+        plotutils.top_left_stamp(title_text, mapper,
+                                 fontsize='medium',
+                                 weight='bold',
+                                 color='black',
+                                 path_effects=[patheffects.withStroke(linewidth=3, foreground="white")])
         plt.show()
 
 
--- weatherpy/plotutils.py
+++ weatherpy/plotutils.py
@@ -13,3 +13,13 @@ def save_image_no_border(ax, saveloc, dpi=None):
     ax.set_frame_on(False)
     plt.axis('off')
     pylab.savefig(saveloc, bbox_inches='tight', pad_inches=0, transparent=True, dpi=dpi)
+
+
+def top_left_stamp(txt, mapper, **text_kwargs):
+    x = 0.01
+    y = 1.0 - x
+    return mapper.ax.text(x, y, txt,
+                          transform=mapper.ax.transAxes,
+                          horizontalalignment='left',
+                          verticalalignment='top',
+                          **text_kwargs)
