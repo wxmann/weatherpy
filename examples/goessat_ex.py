80f61a152cb5db897302bdbc1e49380feca289af
wxmann <wxfreakk91@gmail.com>
2017-01-31 23:20:53 -0800

    Enhancements to projection module. Complete "batch save" images example.

--- examples/goessat_ex.py
+++ examples/goessat_ex.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, time, datetime, timedelta
 
 import matplotlib.pyplot as plt
 from matplotlib import patheffects
@@ -8,26 +8,56 @@ from weatherpy import goessat
 from weatherpy import plotutils
 
 
-def latest_east_conus_ir():
+def latest_east_coast_wv():
     colortable = colortables.wv_accuwx
-    req = goessat.DataRequest('WV', 'EAST-CONUS_4km', date(2017, 1, 20))
+    req = goessat.DataRequest('WV', 'EAST-CONUS_4km')
 
-    with req[datetime(2017, 1, 20, 5, 45)] as plotter:
+    with req[-1] as plotter:
         mapper = plotter.mapper
-        mapper.extent = (-112.5, -70, 17.5, 52.5)
+        mapper.extent = (-112.5, -70, 18.5, 52.5)
         mapper.draw_coastlines()
         mapper.draw_borders()
         mapper.draw_states()
 
         plotter.make_plot(mapper=mapper, colortable=colortable)
-        title_text = 'GOES {}\n{}'.format(plotter.sattype, plotter.timestamp.strftime('%Y %b %d\n%H:%M UTC'))
+        title_text = 'GOES-E {}\n{}'.format(plotter.sattype, plotter.timestamp.strftime('%Y %b %d\n%H:%M UTC'))
         plotutils.top_left_stamp(title_text, mapper,
                                  fontsize='medium',
                                  weight='bold',
                                  color='black',
-                                 path_effects=[patheffects.withStroke(linewidth=3, foreground="white")])
+                                 path_effects=[patheffects.withStroke(linewidth=1.5, foreground="white")])
         plt.show()
 
 
+def save_bunch_of_images_1():
+    colortable = colortables.wv_accuwx
+    req_date = date(2017, 1, 23)
+    req = goessat.DataRequest('WV', 'EAST-CONUS_4km', req_date)
+
+    t0 = datetime.combine(req_date, time(hour=0, minute=45))
+
+    for dhour in range(0, 12):
+        with req[t0 + timedelta(hours=dhour)] as plotter:
+            mapper = plotter.mapper
+            mapper.extent = (-112.5, -70, 18.5, 52.5)
+            mapper.draw_coastlines()
+            mapper.draw_borders()
+            mapper.draw_states()
+
+            plotter.make_plot(mapper=mapper, colortable=colortable)
+            title_text = 'GOES-E {}\n{}'.format(plotter.sattype, plotter.timestamp.strftime('%Y %b %d\n%H:%M UTC'))
+            plotutils.top_left_stamp(title_text, mapper,
+                                     fontsize='small',
+                                     weight='normal',
+                                     color='black',
+                                     path_effects=[patheffects.withStroke(linewidth=1.5, foreground="white")])
+
+            filename = 'wveast-{}.png'.format(plotter.timestamp.strftime('%Y%d%m%H%M'))
+            directory = r'your/directory/here/'
+            plotutils.save_image_no_border(mapper.ax,
+                                           directory + filename,
+                                           dpi=175)
+            plt.clf()
+
 if __name__ == '__main__':
-    latest_east_conus_ir()
+    save_bunch_of_images_1()
--- weatherpy/mapproj.py
+++ weatherpy/mapproj.py
@@ -7,7 +7,6 @@ import weatherpy._mapproj_legacy as _legacy
 
 from weatherpy._pyhelpers import coalesce_kwargs
 
-
 # STANDARD MAPS
 # TODO: do we plan to use them anywhere?
 # atlantic_basin = EquidistantCylindrical(llcrnlat=5.0, llcrnlon=-105.0, urcrnlat=60, urcrnlon=-5.0)
@@ -18,10 +17,20 @@ LambertConformal = _legacy.LambertConformal
 
 
 class CartopyMapProjection(object):
+    _default_line_width = 0.5
+    _default_line_color = 'black'
+    _default_resolution = '50m'
+
     def __init__(self, *args, **kwargs):
         self._crs = self._get_crs(*args, **kwargs)
         self._extent = None
         self._ax = None
+        self._line_properties = {
+            'color': CartopyMapProjection._default_line_color,
+            'width': CartopyMapProjection._default_line_width,
+            'resolution': CartopyMapProjection._default_resolution
+        }
+        self._extent_set = False
 
     def _get_crs(self, *args, **kwargs):
         raise NotImplementedError("CRS implementations are delegated to subclasses.")
@@ -36,43 +45,59 @@ class CartopyMapProjection(object):
 
     @extent.setter
     def extent(self, extent_coord):
-        if extent_coord is None:
-            self._extent = None
-            if self._ax is not None:
-                self._ax.set_global()
-        else:
-            if len(extent_coord) != 4:
-                raise ValueError("Must have four coordinates for extent box")
-            self._extent = tuple(extent_coord)
-            if self._ax is not None:
-                self._ax.set_extent(self._extent)
+        if extent_coord is None or len(extent_coord) != 4:
+            raise ValueError("Extent must be of form (x0, x1, y0, y1)")
+        self._extent = tuple(extent_coord)
+        self._extent_set = False
+        self._set_extent()
+
+    def _set_extent(self):
+        if all((
+            self._ax is not None,
+            not self._extent_set,
+            self._extent is not None
+        )):
+            self._ax.set_extent(self._extent)
+            self._extent_set = True
 
     @property
     def ax(self):
         return self._ax
 
+    def set_line_property(self, prop, val):
+        if prop in self._line_properties:
+            self._line_properties[prop] = val
+        else:
+            warnings.warn("There is no line property: {}. Nothing has been set.".format(prop))
+
+    def reset_drawing(self):
+        self._ax = plt.axes(projection=self.crs)
+
     def initialize_drawing(self):
         if not self._is_drawing_initialized():
             self._ax = plt.axes(projection=self.crs)
-            if self._extent is not None:
-                self._ax.set_extent(self._extent)
+            self._set_extent()
 
     def _is_drawing_initialized(self):
         return self._ax is not None
 
-    def draw_coastlines(self, res='50m', **kwargs):
+    def draw_coastlines(self, **kwargs):
         self.initialize_drawing()
-        self._ax.coastlines(**coalesce_kwargs(kwargs, resolution=res, color='black', linewidth='1'))
+        self._ax.coastlines(**coalesce_kwargs(kwargs, resolution=self._line_properties['resolution'],
+                                              color=self._line_properties['color'],
+                                              linewidth=self._line_properties['width']))
 
     def draw_borders(self, **kwargs):
         self.initialize_drawing()
-        self._ax.add_feature(cfeat.BORDERS, **coalesce_kwargs(kwargs, linewidth='1', edgecolor='black'))
+        self._ax.add_feature(cfeat.BORDERS, **coalesce_kwargs(kwargs, edgecolor=self._line_properties['color'],
+                                                              linewidth=self._line_properties['width']))
 
-    def draw_states(self, res='50m', **kwargs):
+    def draw_states(self, **kwargs):
         self.initialize_drawing()
         states = cfeat.NaturalEarthFeature(category='cultural', name='admin_1_states_provinces_lakes',
-                                           scale=res, facecolor='none')
-        self._ax.add_feature(states, **coalesce_kwargs(kwargs, linewidth='0.5'))
+                                           scale=self._line_properties['resolution'], facecolor='none')
+        self._ax.add_feature(states, **coalesce_kwargs(kwargs, edgecolor=self._line_properties['color'],
+                                                       linewidth=self._line_properties['width']))
 
     def draw_gridlines(self, **kwargs):
         self.initialize_drawing()
@@ -86,13 +111,11 @@ class CartopyMapProjection(object):
 
 
 class EquidistantCylindricalMapper(CartopyMapProjection):
-
     def _get_crs(self):
         return ccrs.PlateCarree()
 
 
 class LambertConformalMapper(CartopyMapProjection):
-
     def __init__(self, lat0, lon0, stdlat1=None, stdlat2=None, r_earth=6370997):
         CartopyMapProjection.__init__(self, lat0, lon0, stdlat1, stdlat2, r_earth)
         self._param_metadata = {
