9374dc1de4c1b6e2e219f3b9ebcbb73adc6aacd1
wxmann <wxfreakk91@gmail.com>
2017-04-13 00:00:55 -0700

    Add geopotential height plots.

--- weatherpy/models.py
+++ weatherpy/models.py
@@ -13,7 +13,8 @@ from weatherpy.thredds import DatasetAccessException
 class GfsPlotter(object):
 
     plotvarmap = {
-        uaplots.MSLP: 'prmslmsl'
+        uaplots.MSLP: 'prmslmsl',
+        uaplots.GeopotentialHeight: 'hgtprs'
     }
 
     # TODO: get the basetime from the dataset somehow
@@ -22,23 +23,9 @@ class GfsPlotter(object):
         self._wrapped_data = NcDatasetWrapper(dataset)
         self._basetime = basetime
 
-        self._plot_cls = uaplots.MSLP
         self._lats = self._wrapped_data.dimension_values('lat')
         self._lons = self._wrapped_data.dimension_values('lon')
         self._hr = 0
-        self._level = 'sfc'
-
-    @property
-    def plot(self):
-        return self._plot_cls
-
-    @plot.setter
-    def plot(self, newcls):
-        try:
-            GfsPlotter.plotvarmap[newcls]
-        except KeyError:
-            raise ValueError("Unsupported plotter supplied. Try a different plot")
-        self._plot_cls = newcls
 
     @property
     def hour(self):
@@ -48,40 +35,33 @@ class GfsPlotter(object):
     def hour(self, newhr):
         self._hr = newhr
 
-    @property
-    def plevel(self):
-        return self._level
-
-    @plevel.setter
-    def plevel(self, newplev):
-        if newplev != 'sfc' and not isinstance(newplev, int):
-            raise ValueError("Require a pressure level or \'sfc\'")
-        self._level = newplev
-
     def default_map(self):
         return mappers.LargeScaleMap(ccrs.PlateCarree())
 
-    def make_plot(self, mapper=None, colortable=None):
+    def plotter(self, plots, plevel='sfc', mapper=None):
         if mapper is None:
             mapper = self.default_map()
+
+        if plevel != 'sfc' and not isinstance(plevel, int):
+            raise ValueError("Invalid plevel argument: require a pressure level or \'sfc\'")
 
         lats = self._wrapped_data.dimension_values('lat')
         lons = self._wrapped_data.dimension_values('lon')
 
         kwargs = {'time': self._basetime + timedelta(hours=self.hour)}
-        if self.plevel != 'sfc':
-            kwargs['lev'] = self.plevel
-        data = self._wrapped_data.variable(GfsPlotter.plotvarmap[self.plot])(**kwargs)
-        plot_instance = self.plot(data, lons, lats)
+        if plevel != 'sfc':
+            kwargs['lev'] = plevel
 
-        plot_instance.make_plot(mapper, colortable)
-        return mapper
+        plotter_objs = []
+        if not hasattr(plots, '__iter__'):
+            plots = [plots]
 
+        for plot in plots:
+            data = self._wrapped_data.variable(GfsPlotter.plotvarmap[plot])(**kwargs)
+            plotter_obj = plot(data, lons, lats, mapper)
+            plotter_objs.append(plotter_obj)
 
-def _reindex_lons(lon):
-    if 180 < lon <= 360:
-        return -360 + lon
-    return lon
+        return tuple(plotter_objs) if len(plotter_objs) != 1 else plotter_objs[0]
 
 
 class NcDatasetWrapper(object):
@@ -150,11 +130,15 @@ class NcepModelRequest(object):
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     req = NcepModelRequest('GFS')
-    basetime = datetime(2017, 4, 10, 0, 0)
+    basetime = datetime(2017, 4, 11, 0, 0)
     ds = req[basetime]
-    plotter = GfsPlotter(nc.Dataset(ds), basetime)
-    mapper = plotter.default_map()
+    gfs = GfsPlotter(nc.Dataset(ds), basetime)
+
+    mapper = gfs.default_map()
     mapper.extent = (-130, -60, 20, 55)
-    plotter.make_plot(mapper)
     mapper.draw_default()
+
+    plotter = gfs.plotter(uaplots.GeopotentialHeight, 500, mapper)
+    plotter.contours()
+
     plt.show()
--- weatherpy/uaplots.py
+++ weatherpy/uaplots.py
@@ -3,12 +3,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from weatherpy.calcs import pa2hPa, extrema
+from weatherpy.maps.mappers import MapperBase
 
 
-class MSLP(object):
-    def __init__(self, data, lons, lats, unit='Pa'):
+class BaseUAPlot(object):
+    def __init__(self, data, lons, lats, mapper):
         self._lats = lats
         self._lons = lons
+        self._data = data
+        self._mapper = mapper
+
+    @property
+    def mapper(self):
+        return self._mapper
+
+    @mapper.setter
+    def mapper(self, newmapper):
+        if not isinstance(newmapper, MapperBase):
+            raise ValueError("Provided argument is not a valid mapper")
+        self._mapper = newmapper
+
+
+def label_contours(contours, levels):
+    labels = plt.clabel(contours, levels, fontsize=10, fmt='%1.0f',
+                        inline_spacing=-8, inline=True)
+    for label in labels:
+        label.set_rotation(0)
+
+
+class MSLP(BaseUAPlot):
+    def __init__(self, data, lons, lats, mapper, unit='Pa'):
+        super(MSLP, self).__init__(data, lons, lats, mapper)
         if unit == 'Pa':
             self._data = pa2hPa(data)
         elif unit == 'hPa':
@@ -16,18 +41,16 @@ class MSLP(object):
         else:
             raise ValueError("Unsupported MSLP unit: {}".format(unit))
 
-    def make_plot(self, mapper, colortable=None):
-        contour_lvls = np.arange(900, 1100, 3)
+    def contours(self, levels=None):
+        if levels is None:
+            levels = np.arange(900, 1100, 3)
+        self._mapper.initialize_drawing()
+        contour_lines = self._mapper.ax.contour(self._lons, self._lats, self._data,
+                                                levels, colors='k', linewidths=1,
+                                                transform=ccrs.PlateCarree())
+        label_contours(contour_lines, levels)
 
-        mapper.initialize_drawing()
-        contour_lines = mapper.ax.contour(self._lons, self._lats, self._data,
-                                          contour_lvls, colors='k', linewidths=1,
-                                          transform=ccrs.PlateCarree())
-
-        self._plot_highs_lows()
-        self._label(contour_lines, contour_lvls)
-
-    def _plot_highs_lows(self):
+    def high_low_centers(self):
         lon_mesh, lat_mesh = np.meshgrid(self._lons, self._lats)
         local_min, local_max = extrema(self._data, mode='wrap', window=50)
         latlows, lonlows = lat_mesh[local_min], lon_mesh[local_min]
@@ -58,9 +81,17 @@ class MSLP(object):
         _do_plot(lonlows, latlows, lowvals, 'L', 'red')
         _do_plot(lonhighs, lathighs, highvals, 'H', 'blue')
 
-    def _label(self, contour_lines, contour_lvls):
-        labels = plt.clabel(contour_lines, contour_lvls, fontsize=10, fmt='%1.0f',
-                            inline_spacing=-8, inline=True)
-        for label in labels:
-            label.set_rotation(0)
 
+class GeopotentialHeight(BaseUAPlot):
+    def __init__(self, data, lons, lats, mapper):
+        super(GeopotentialHeight, self).__init__(data, lons, lats, mapper)
+        self._data /= 10.0
+
+    def contours(self, levels=None):
+        if levels is None:
+            levels = np.arange(420, 630, 6)
+        self._mapper.initialize_drawing()
+        contour_lines = self._mapper.ax.contour(self._lons, self._lats, self._data,
+                                                levels, colors='k', linewidths=1,
+                                                transform=ccrs.PlateCarree())
+        label_contours(contour_lines, levels)
