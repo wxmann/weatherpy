12618ab57786f261130ce0fc53855746fac79703
wxmann <wxfreakk91@gmail.com>
2017-05-07 01:50:34 -0700

    Add additional CONUS extents.

--- weatherpy/maps/extents.py
+++ weatherpy/maps/extents.py
@@ -3,6 +3,25 @@ conus = (-127.5, -65.5, 20.5, 52)
 
 us_southeast = (-98, -74, 23, 40)
 
-us_southctrl = (-110, -88, 25, 41)
+us_southctrl = (-110, -88, 24.5, 41)
 
-us_southwest = (-128.5, -98, 27, 42.5)
+us_southwest = (-128, -98, 28.5, 42.5)
+
+us_northwest = (-130, -102.5, 39, 52)
+
+us_northctrl = (-110, -85.5, 38, 52)
+
+us_northeast = (-90, -65, 36, 52)
+
+southern_plains = (-107.5, -91, 27.5, 39.5)
+
+central_plains = (-107.5, -91, 33.5, 44)
+
+northern_plains = (-108.5, -90, 41, 51.5)
+
+# dixie = (-97.5, -75.5, 27.5, 37.5)
+dixie = (-97, -79, 28, 37.5)
+
+midwest = (-98, -79, 36, 50)
+
+gulf_of_mexico = (-100, -79, 17.5, 33)
--- weatherpy/maps/tests/test_extents.py
+++ weatherpy/maps/tests/test_extents.py
@@ -11,32 +11,95 @@ from weatherpy.maps import extents
 @pytest.mark.mpl_image_compare
 def test_conus_region():
     fig = plt.figure()
-    _draw_maps_with_region(extents.conus)
+    _draw_maps_with_us_region(extents.conus)
     return fig
 
 
 @pytest.mark.mpl_image_compare
 def test_us_southeast_region():
     fig = plt.figure()
-    _draw_maps_with_region(extents.us_southeast)
+    _draw_maps_with_us_region(extents.us_southeast)
     return fig
 
 
 @pytest.mark.mpl_image_compare
 def test_us_southcentral_region():
     fig = plt.figure()
-    _draw_maps_with_region(extents.us_southctrl)
+    _draw_maps_with_us_region(extents.us_southctrl)
     return fig
 
 
 @pytest.mark.mpl_image_compare
 def test_us_southwest_region():
     fig = plt.figure()
-    _draw_maps_with_region(extents.us_southwest)
+    _draw_maps_with_us_region(extents.us_southwest)
     return fig
 
 
-def _draw_maps_with_region(region):
+@pytest.mark.mpl_image_compare
+def test_us_northwest_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.us_northwest)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_us_northcentral_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.us_northctrl)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_us_northeast_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.us_northeast)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_southern_plains_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.southern_plains)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_central_plains_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.central_plains)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_northern_plains_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.northern_plains)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_dixie_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.dixie)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_midwest_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.midwest)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_gulf_of_mexico_region():
+    fig = plt.figure()
+    _draw_maps_with_us_region(extents.gulf_of_mexico)
+    return fig
+
+
+def _draw_maps_with_us_region(region):
     for i, proj in enumerate([ccrs.PlateCarree(),
                              ccrs.AlbersEqualArea(central_longitude=-100.0),
                              maps.projections.goes_east_nearside(),
