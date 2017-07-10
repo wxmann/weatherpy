d777b062558d69482c9e3193d94dcb1b8a23b613
wxmann <jim.z.tang@gmail.com>
2017-07-10 01:22:03 -0700

    [GOES-16-SAT] mpl tests

--- weatherpy/maps/tests/test_extents.py
+++ weatherpy/maps/tests/test_extents.py
@@ -8,119 +8,119 @@ import matplotlib.pyplot as plt
 from weatherpy.maps import extents
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_conus_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.conus)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_us_southeast_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.us_southeast)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_us_southcentral_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.us_southctrl)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_us_southwest_region():
     fig = plt.figure()
     _draw_maps_with_west_coast_us_region(extents.us_southwest)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_us_northwest_region():
     fig = plt.figure()
     _draw_maps_with_west_coast_us_region(extents.us_northwest)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_us_northcentral_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.us_northctrl)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_us_northeast_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.us_northeast)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_southern_plains_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.southern_plains)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_central_plains_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.central_plains)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_northern_plains_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.northern_plains)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_dixie_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.dixie)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_midwest_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.midwest)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_gulf_of_mexico_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.gulf_of_mexico)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_florida_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.florida)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_carolinas_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.carolinas)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_northeast_megalopolis_region():
     fig = plt.figure()
     _draw_maps_with_eastern_us_region(extents.northeast_megalopolis)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=20)
 def test_california_region():
     fig = plt.figure()
     _draw_maps_with_west_coast_us_region(extents.california)
---
+++ weatherpy/satellite/tests/test_goes16_plot.py
@@ -0,0 +1,105 @@
+import os
+
+import matplotlib.pyplot as plt
+import netCDF4
+import pytest
+
+import config
+from weatherpy import ctables
+from weatherpy.maps import extents
+from weatherpy.satellite.goes16 import Goes16Plotter
+
+VISIBLE_SECTOR_FILE = 'GOES16_Mesoscale-1_20170628_235927_0.64_500m_41.8N_95.6W_Ch02.nc4'
+INFRARED_SECTOR_FILE = 'GOES16_Mesoscale-1_20170628_235927_11.20_2km_41.8N_95.6W_Ch14.nc4'
+WV_SECTOR_FILE = 'GOES16_Mesoscale-1_20170628_235927_6.19_2km_41.8N_95.6W_Ch08.nc4'
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_visible():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, VISIBLE_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((41.6, -93.6), km=300)
+        mapper, _ = plotter.make_plot(extent=extent)
+        mapper.draw_default()
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_infrared_default_ctable():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, INFRARED_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((41.6, -93.6), km=300)
+        mapper, _ = plotter.make_plot(extent=extent)
+        mapper.draw_default()
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_infrared_rainbow_ctable():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, INFRARED_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((41.6, -93.6), km=300)
+        mapper, _ = plotter.make_plot(colortable=ctables.ir.rainbow, extent=extent)
+        mapper.draw_default()
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_extent_larger_than_data_region():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, INFRARED_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((41.6, -93.6), km=1000)
+        mapper, _ = plotter.make_plot(extent=extent)
+        mapper.draw_default()
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_outside_of_data_region():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, INFRARED_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((37.8, -122.4), km=300)
+        mapper, _ = plotter.make_plot(extent=extent)
+        mapper.draw_default()
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20, filename='test_plot_infrared_default_ctable.png')
+def test_plot_no_strict_mode():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, INFRARED_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((41.6, -93.6), km=300)
+        mapper, _ = plotter.make_plot(extent=extent, strict=False)
+        mapper.draw_default()
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_wv_channel_default_ctable():
+    fig = plt.figure()
+    dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, WV_SECTOR_FILE]))
+
+    with Goes16Plotter(dataset) as plotter:
+        extent = extents.zoom((41.6, -93.6), km=300)
+        mapper, _ = plotter.make_plot(extent=extent)
+        mapper.draw_default()
+
+    return fig
