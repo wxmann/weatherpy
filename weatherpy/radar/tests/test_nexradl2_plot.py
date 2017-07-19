413df5b95a14e48ddf58c1ad7b3fa48473d00310
wxmann <jim.z.tang@gmail.com>
2017-07-19 00:38:21 -0700

    [NEXRAD L2] Image comparison tests

---
+++ weatherpy/radar/tests/test_nexradl2_plot.py
@@ -0,0 +1,76 @@
+import os
+
+import netCDF4
+import pytest
+import matplotlib.pyplot as plt
+
+import config
+from weatherpy import maps
+from weatherpy.radar.nexradl2 import Nexrad2Plotter
+
+RADAR_FILE = 'Level2_KGLD_20170713_0200.nc'
+
+radar_dataset = None
+
+
+@pytest.fixture(scope='module', autouse=True)
+def lazyloaddataset():
+    print('loading...')
+    global radar_dataset
+    radar_dataset = netCDF4.Dataset(os.sep.join([config.TEST_DATA_DIR, RADAR_FILE]))
+    yield
+    print('closing...')
+    radar_dataset.close()
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_reflectivity():
+    fig = plt.figure()
+    _run_basic_test_with('Reflectivity')
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_velocity():
+    fig = plt.figure()
+    _run_basic_test_with('RadialVelocity')
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_reflectivity_with_limiting_range_ring():
+    fig = plt.figure()
+    plotter = Nexrad2Plotter(radar_dataset)
+    orig_map = plotter.default_map()
+    map_to_use = maps.LargeScaleMap(orig_map.crs)
+    map_to_use.extent = orig_map.extent
+
+    mapper, _ = plotter.make_plot(mapper=map_to_use)
+    mapper.draw_default()
+    plotter.range_ring(mapper)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=20)
+def test_plot_reflectivity_with_nonlimiting_range_ring():
+    fig = plt.figure()
+    plotter = Nexrad2Plotter(radar_dataset)
+    orig_map = plotter.default_map()
+    map_to_use = maps.LargeScaleMap(orig_map.crs)
+    map_to_use.extent = orig_map.extent
+
+    mapper, _ = plotter.make_plot(mapper=map_to_use)
+    mapper.draw_default()
+    plotter.range_ring(mapper, limit=False, fit_to_ring=False)
+    return fig
+
+
+def _run_basic_test_with(radartype=None, hires=True, sweep=0):
+    plotter = Nexrad2Plotter(radar_dataset)
+    orig_map = plotter.default_map()
+    map_to_use = maps.LargeScaleMap(orig_map.crs)
+    map_to_use.extent = orig_map.extent
+
+    plotter.set_radar(radartype, hires, sweep)
+    mapper, _ = plotter.make_plot(mapper=map_to_use)
+    mapper.draw_default()
